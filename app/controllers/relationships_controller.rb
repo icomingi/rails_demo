5b886f3b931e4a31a3af57aff488803f00f52118
Josh Erich <josherichchen@gmail.com>
2013-04-05 23:19:12 +0800

    add plus and cancel button to route_record

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,21 @@
+class RelationshipsController < ApplicationController
+	before_filter :authenticate
+
+	def create
+		@route_record = RouteRecord.find_by_id(params[:relationship][:route_id])
+		current_user.follow!(@route_record)
+		respond_to do |format|
+			format.html { redirect_to @route_record }
+			format.js
+		end
+	end
+
+	def destroy
+		@route_record = Relationship.find(params[:id]).route
+		current_user.unfollow!(@route_record)
+		respond_to do |format|
+			format.html { redirect_to @route_record }
+			format.js
+		end
+	end
+end
